2ac7a412d394d66cf024b2307dbd00a1fd9349ba
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-03-18 05:28:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,31 +2,25 @@
 
 if [ "$(id -u)" -eq 0 ]
 then
-    echo "Need an unprivileged user to run!"
+    echo "Privileged access is not allowed!"
     exit 1
 fi
 
-# Install Oh My Zsh
+# Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Download Oh My Zsh plugins
+# Oh My Zsh Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-# Download Powerlevel10k theme
+# Powerlevel10k Theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-# Copy files
-cp -r dotfiles/. ~
+cp -r homedir/. ~
 
 # Build font information cache files
 fc-cache -fv
 
-# Enable the corresponding devices
-rfkill unblock wlan
-
-# Disable the corresponding devices
 rfkill block bluetooth
 
-# Enable timers
 systemctl --user enable battery-notification.timer

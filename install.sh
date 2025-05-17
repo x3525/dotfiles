b6d619322f7bcf4c27b3604101be27ccf7e397d6
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-05-17 16:08:50 +0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ then
     exit 1
 fi
 
+rfkill block bluetooth
+
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# shellcheck disable=SC1090
+. <(head -n "$(grep -n ZSH= ~/.zshrc | cut -c 1)" ~/.zshrc)
+
 # Oh My Zsh Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH"/plugins/zsh-syntax-highlighting
 
 # Oh My Posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
@@ -20,7 +25,5 @@ cp -r .[!g]* ~
 
 # Build font information cache files
 fc-cache -fv
-
-rfkill block bluetooth
 
 systemctl --user enable battery-notification.timer

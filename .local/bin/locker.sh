042218499ad610e0746103ca4342f2990aa93a3e
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-05-17 16:51:47 +0300

    Change scripts to C2

--- .local/bin/locker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dunstctl set-paused true
-
-i3lock --nofork --color=000000 --pointer=default --ignore-empty-password --show-failed-attempts --show-keyboard-layout
-
-dunstctl set-paused false

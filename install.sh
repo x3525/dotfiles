ad3b7ab22e25f82339c88b3c748d964cb16d7302
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-05-22 18:03:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,9 @@ rfkill block bluetooth
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# shellcheck disable=SC1090
-. <(head -n "$(grep -n ZSH= ~/.zshrc | cut -c 1)" ~/.zshrc)
-
 # Oh My Zsh Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH"/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
 # Oh My Posh
 curl -s https://ohmyposh.dev/install.sh | bash -s

7226594d1ab70cbfcc70d6d4ab1f1ab6693d514d
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-06-07 21:47:50 +0300

    Remove an alias from aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,3 @@
 alias cat='bat --plain --plain --force-colorization --theme="Solarized (dark)"'
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
-alias rm='trash-put'

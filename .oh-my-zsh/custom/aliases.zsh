70f608650fafcd663431dec5a2528873f8f2f596
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-06-18 08:01:34 +0300

    Fix aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,3 @@
-alias cat='bat --plain --plain --force-colorization --theme="Solarized (dark)"'
+alias cat='bat --plain --plain --theme="Solarized (dark)"'
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'

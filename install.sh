1cbc36494c3c77d39f81f5bbd15bbba41cf55dc2
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-06-25 01:44:59 +0300

    Improve? install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM
 # Oh My Posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
-cp -r .[!g]* ~
+cp -r -- .[!g]* ~
 
 # Build font information cache files
 fc-cache -fv

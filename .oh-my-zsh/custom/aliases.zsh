21039d0a814b63b1364a9665f090bf5afda4325c
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-06-25 19:51:35 +0300

    Add new aliases and unalias not needed ones

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,13 @@
+unalias -a
+alias -g ...=../..
+alias -g ....=../../..
+alias -g .....=../../../..
+alias -g ......=../../../../..
 alias cat='bat --plain --plain --theme="Solarized (dark)"'
+alias destroy='print -n -u 2 >| "$HISTFILE"; fc -p "$HISTFILE"'
+alias grep='grep --color=auto'
+alias la='ls --color=tty -l --human-readable --all'
+alias ll='ls --color=tty -l --human-readable'
+alias ls='ls --color=tty'
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'

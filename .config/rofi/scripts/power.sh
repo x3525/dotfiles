5a8e9f8338bdabfd4510218682e68f516856ade5
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-07-14 04:50:48 +0300

    Improve Rofi power.sh

--- .config/rofi/scripts/power.sh
+++ .config/rofi/scripts/power.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -en "Sign out\0icon\x1fsystem-log-out\n"
-echo -en "Restart\0icon\x1fsystem-reboot\n"
-echo -en "Shut down\0icon\x1fsystem-shutdown\n"
+echo -en "Sign out\0permanent\x1ftrue\x1ficon\x1fsystem-log-out\n"
+echo -en "Restart\0permanent\x1ftrue\x1ficon\x1fsystem-reboot\n"
+echo -en "Shut down\0permanent\x1ftrue\x1ficon\x1fsystem-shutdown\n"
 
 case $1 in
     "Sign out")

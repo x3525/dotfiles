4e5851e7ab32284efcf4149f910a7a4c8074392c
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-07-25 19:01:38 +0300

    Update aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,8 +5,10 @@ alias -g .....=../../../..
 alias -g ......=../../../../..
 alias cat='bat --plain --plain --theme="Solarized (dark)"'
 alias grep='grep --color=auto'
+alias history='fc -i -l 1'
+alias history_clear='print -n -u 2 >| "$HISTFILE" && fc -p "$HISTFILE"'
 alias la='ls --color=tty -l --human-readable --all'
 alias ll='ls --color=tty -l --human-readable'
 alias ls='ls --color=tty'
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
+alias xc='xsel --clipboard --input'
+alias xp='xsel --clipboard --output'

fc0ff5eb62a47305d07a428a656a60248e1e094d
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-08-31 08:57:04 +0300

    Update .zsh/60-aliases.zsh

--- .zsh/60-aliases.zsh
+++ .zsh/60-aliases.zsh
@@ -1,7 +1,9 @@
 alias cat='bat --style=plain --paging=never --theme="Solarized (dark)"'
 alias clear=
+alias cp='cp --interactive'
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
+alias mv='mv --interactive'
 alias rm=trash-put
 alias xc='xsel --clipboard --input'
 alias xp='xsel --clipboard --output'

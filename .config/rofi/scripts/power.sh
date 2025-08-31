5f67e42f312f5359b5dfdd330ed29dbe450cca28
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-08-31 14:21:56 +0300

    Prefix all possible commands with command

--- .config/rofi/scripts/power.sh
+++ .config/rofi/scripts/power.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -en "poweroff\0icon\x1fsystem-shut-down\x1fdisplay\x1fShut Down\x1fpermanent\x1ftrue\x1finfo\x1fsystemctl\n"
-echo -en "reboot\0icon\x1fsystem-restart\x1fdisplay\x1fRestart\x1fpermanent\x1ftrue\x1finfo\x1fsystemctl\n"
-echo -en "exit\0icon\x1fsystem-log-out\x1fdisplay\x1fLog Out\x1fpermanent\x1ftrue\x1finfo\x1fi3-msg\n"
+command echo -en "poweroff\0icon\x1fsystem-shut-down\x1fdisplay\x1fShut Down\x1fpermanent\x1ftrue\x1finfo\x1fsystemctl\n"
+command echo -en "reboot\0icon\x1fsystem-restart\x1fdisplay\x1fRestart\x1fpermanent\x1ftrue\x1finfo\x1fsystemctl\n"
+command echo -en "exit\0icon\x1fsystem-log-out\x1fdisplay\x1fLog Out\x1fpermanent\x1ftrue\x1finfo\x1fi3-msg\n"
 
 [ -z "$ROFI_INFO" ] || $ROFI_INFO "$1"
--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 if [ "$(id -u)" -eq 0 ]
 then
-    echo "Privileged access is not allowed!"
+    command echo "Privileged access is not allowed!"
     exit 1
 fi
 
 # Oh My Posh
-curl -s https://ohmyposh.dev/install.sh | bash -s
+command curl -s https://ohmyposh.dev/install.sh | command bash -s
 
-cp -r -- .[!g]* ~
+command cp -r -- .[!g]* ~
 
 # Build font information cache files
-fc-cache -fv
+command fc-cache -fv
 
 # Enable timers
-systemctl --user enable battery-notification.timer
+command systemctl --user enable battery-notification.timer

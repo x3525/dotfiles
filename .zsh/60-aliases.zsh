ff11389e711d3277233e70b62fe8dc9360998015
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-08-31 11:51:49 +0300

    Remove aliases: cp mv

--- .zsh/60-aliases.zsh
+++ .zsh/60-aliases.zsh
@@ -1,9 +1,7 @@
 alias cat='bat --style=plain --paging=never --theme="Solarized (dark)"'
 alias clear=
-alias cp='cp --interactive'
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
-alias mv='mv --interactive'
 alias rm=trash-put
 alias xc='xsel --clipboard --input'
 alias xp='xsel --clipboard --output'

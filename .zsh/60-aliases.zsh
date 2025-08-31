ff4be2d85037628a8975d223d8e4a994b9268704
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-08-31 07:02:32 +0300

    Change 60-aliases.zsh

--- .zsh/60-aliases.zsh
+++ .zsh/60-aliases.zsh
@@ -4,6 +4,6 @@ alias cat='bat --style=plain --paging=never --theme="Solarized (dark)"'
 alias clear=
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
-alias rm='trash-put --verbose'
+alias rm=trash-put
 alias xc='xsel --clipboard --input'
 alias xp='xsel --clipboard --output'

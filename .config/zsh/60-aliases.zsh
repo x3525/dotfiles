2231e3cce598f4a0ef98e7b82a6041e6dd60d63a
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-09-25 07:35:34 +0300

    Change 60-aliases.zsh

--- .config/zsh/60-aliases.zsh
+++ .config/zsh/60-aliases.zsh
@@ -1,6 +1,5 @@
 alias cat='bat --style=plain --paging=never --theme="Solarized (dark)"'
 alias diff='diff --color=auto'
-alias eject='udisksctl power-off'
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
 alias rm=trash-put

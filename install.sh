e7d2bfbb70d927a23217ffd7f434843b34ea1f4a
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-09-28 20:58:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$(command id -u)" -eq 0 ]
+if [ "$(id -u)" -eq 0 ]
 then
-    command echo "Privileged access is not allowed!"
+    echo "Privileged access is not allowed!"
     exit 1
 fi
 
-command cp -r -- .[!g]* ~
+cp -r -- .[!g]* ~
 
 # Enable timers
-command systemctl --user enable battery-notification.timer
+systemctl --user enable battery-notification.timer
